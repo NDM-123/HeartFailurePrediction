c94c6728a29f6483a4bb6fe429d8a394cb0cae06
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2022-02-06 23:45:43 +0200

    describes prints...

--- heartPred.py
+++ heartPred.py
@@ -10,16 +10,17 @@ from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
-from IPython.display import display
+from IPython.display import display as disDF
 import seaborn as sns
-import numpy
+import matplotlib. pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
 
 def plotFeatures(df):
-    sns.pairplot(df)
-    # sns.pairplot(df, hue="HeartDisease" )
+    sns.pairplot(df, hue="HeartDisease", diag_kws={'bw': 0.2}, plot_kws={"s": 40})
+    plt.savefig("try.png")
+    # plt.show()
 
 def modelPrediction(model,X_train,X_test,y_train, y_test):
     model.fit(X_train, y_train)
@@ -30,11 +31,12 @@ def modelPrediction(model,X_train,X_test,y_train, y_test):
 
     test_acc = accuracy_score(y_test, y_test_pred)
     train_acc = accuracy_score(y_train, y_train_pred)
-    print(type(model).__name__)
-    print("cross validation: ", cv.mean())
-    print("Train set Accuracy: ", train_acc)
-    print("Test set Accuracy: ", test_acc)
-    print()
+
+    # print(type(model).__name__)
+    # print("cross validation: ", cv.mean())
+    # print("Train set Accuracy: ", train_acc)
+    # print("Test set Accuracy: ", test_acc)
+    # print()
     return test_acc
 
 def data_process():
@@ -59,7 +61,7 @@ def models(X_train, X_test, y_train, y_test):
     models.append(modelPrediction(DecisionTreeClassifier(max_depth=6), X_train, X_test, y_train, y_test))
     # n_estimators=1 == Decision tree
     models.append(modelPrediction(AdaBoostClassifier(n_estimators=5, base_estimator=DecisionTreeClassifier(max_depth=5), learning_rate=0.001),X_train, X_test, y_train, y_test))
-    models.append(modelPrediction(MLPClassifier(hidden_layer_sizes=16, learning_rate_init=0.01, random_state=0, solver='adam'), X_train, X_test, y_train, y_test))
+    models.append(modelPrediction(MLPClassifier(hidden_layer_sizes=32, learning_rate_init=0.01, random_state=0, solver='adam'), X_train, X_test, y_train, y_test))
     models.append(modelPrediction(KNeighborsClassifier(n_neighbors=8, algorithm='brute'), X_train, X_test, y_train, y_test))
     models.append(modelPrediction(SVC(), X_train, X_test, y_train, y_test))
     return models
@@ -69,12 +71,14 @@ def modelsFeatureSelection(x,y,testPercent):
     bestTestAccuracy = [0,0,0,0,0]
     bestNumberOfFeatures = [0,0,0,0,0]
 
+    bestFeaturesMask = [0]*5
+
+
     for k in range(1, x.shape[1]):
-        # answers = []
         skb = SelectKBest(chi2, k=k)
         skb.fit(x, y)
         X_new = skb.transform(x)
-        X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=testPercent, random_state=0)
+        X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size = testPercent, random_state=0)
 
 
         answers = models(X_train, X_test, y_train, y_test)
@@ -83,9 +87,9 @@ def modelsFeatureSelection(x,y,testPercent):
                 if answers[i] > bestTestAccuracy[i]:
                     bestTestAccuracy[i] = answers[i]
                     bestNumberOfFeatures[i] = k
+                    bestFeaturesMask[i] = skb.get_support()
 
-
-    return bestTestAccuracy,bestNumberOfFeatures
+    return bestTestAccuracy,bestNumberOfFeatures,bestFeaturesMask
 
 
 
@@ -99,7 +103,7 @@ if __name__ == '__main__':
 
 
         # base model
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=0)
         modelsWithFeatures = models(X_train, X_test, y_train, y_test)
 
 
@@ -108,22 +112,34 @@ if __name__ == '__main__':
 
             bestOfModels.append(models(X_train, X_test, y_train, y_test))
 
-        best = pd.DataFrame(bestOfModels)
 
 
-        bestTestAccuracy, bestNumberOfFeatures = modelsFeatureSelection(x, y, 0.2)
+        bestTestAccuracy, bestNumberOfFeatures,bestFeaturesMask = modelsFeatureSelection(x, y, 0.1)
+
+
+        best = pd.DataFrame(bestOfModels, columns = ['Decision tree', 'AdaBoost','MLP', 'KNN', 'SVM'])
+
 
         # printing test size from 0.1-1
-        display(best)
+        disDF(best)
 
-        best.describe()
+        # best.describe(include='all').T.style.background_gradient(subset=['mean', 'std', '50%', 'count'], cmap='RdPu')
+        print(best.describe())
 
-        print(bestOfModels)
 
         # how come some features without feature selection get same score? KNN...
+        print('models With all Features:')
+        print("Decision Tree:\t\t Adaboost:\t\t\tMLP:\t\t\t\t KNN:\t\t\t\tSVM:")
+        print(bestTestAccuracy)
+
+        print('models With Features:')
         print("Decision Tree:\t\t Adaboost:\t\t\tMLP:\t\t\t\t KNN:\t\t\t\tSVM:")
         print(modelsWithFeatures)
-        print(bestTestAccuracy)
         print(bestNumberOfFeatures)
+
+        featuresSelected = pd.dataFrame(bestFeaturesMask)
+        disDF(featuresSelected)
+
+
 
         # plotModels()
