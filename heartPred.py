332c31542ac576ce5c91eb007d3ae601f17929cf
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2022-02-09 00:42:30 +0200

    code small updates, research challenges explained

--- heartPred.py
+++ heartPred.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neural_network import MLPClassifier
@@ -20,6 +21,7 @@ warnings.filterwarnings("ignore")
 def plotFeatures(df):
     sns.pairplot(df, hue="HeartDisease", diag_kws={'bw': 0.2}, plot_kws={"s": 40})
     plt.savefig("try.png")
+
     # plt.show()
 
 def modelPrediction(model,X_train,X_test,y_train, y_test):
@@ -27,8 +29,9 @@ def modelPrediction(model,X_train,X_test,y_train, y_test):
 
     y_train_pred = model.predict(X_train)
     y_test_pred = model.predict(X_test)
-    cv = cross_val_score(model, X_train, y_train, cv=10, scoring='accuracy')
 
+
+    cv = cross_val_score(model, X_train, y_train, cv=10, scoring='accuracy')
     test_acc = accuracy_score(y_test, y_test_pred)
     train_acc = accuracy_score(y_train, y_train_pred)
 
@@ -43,12 +46,15 @@ def data_process():
     df = pd.read_csv(r'heart.csv')
 
     # optional
-    plotFeatures(df)
 
-    # label encoding
+    # label encoding + normalizing the data, not the best method as its better to use hot encode rather than label encoder
     label_encoder = preprocessing.LabelEncoder()
     for i in df:
         df[i] = label_encoder.fit_transform(df[i])
+
+    plotFeatures(df)
+
+    print(df.describe().T)
 
     x = df.drop('HeartDisease', axis=1)
     y = df['HeartDisease']
@@ -58,12 +64,14 @@ def data_process():
 def models(X_train, X_test, y_train, y_test):
     models = []
 
-    models.append(modelPrediction(DecisionTreeClassifier(max_depth=6), X_train, X_test, y_train, y_test))
+    models.append(modelPrediction(DecisionTreeClassifier(max_depth=6, random_state = 42), X_train, X_test, y_train, y_test))
     # n_estimators=1 == Decision tree
-    models.append(modelPrediction(AdaBoostClassifier(n_estimators=5, base_estimator=DecisionTreeClassifier(max_depth=5), learning_rate=0.001),X_train, X_test, y_train, y_test))
+    models.append(modelPrediction(AdaBoostClassifier(n_estimators=5, base_estimator=DecisionTreeClassifier(max_depth=6), learning_rate=0.001,random_state = 42),X_train, X_test, y_train, y_test))
     models.append(modelPrediction(MLPClassifier(hidden_layer_sizes=32, learning_rate_init=0.01, random_state=0, solver='adam'), X_train, X_test, y_train, y_test))
     models.append(modelPrediction(KNeighborsClassifier(n_neighbors=8, algorithm='brute'), X_train, X_test, y_train, y_test))
     models.append(modelPrediction(SVC(), X_train, X_test, y_train, y_test))
+    # print("GNB: ")
+    # print(modelPrediction(GaussianNB(),X_train, X_test, y_train, y_test))
     return models
 
 def modelsFeatureSelection(x,y,testPercent):
@@ -103,8 +111,8 @@ if __name__ == '__main__':
 
 
         # base model
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=0)
-        modelsWithFeatures = models(X_train, X_test, y_train, y_test)
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+        modelsWithAllFeatures = models(X_train, X_test, y_train, y_test)
 
 
         for j in [x * 0.1 for x in range(1, 10)]:
@@ -114,10 +122,11 @@ if __name__ == '__main__':
 
 
 
-        bestTestAccuracy, bestNumberOfFeatures,bestFeaturesMask = modelsFeatureSelection(x, y, 0.1)
+        bestTestAccuracyAfterFS, bestNumberOfFeatures, bestFeaturesMask = modelsFeatureSelection(x, y, 0.2)
 
 
         best = pd.DataFrame(bestOfModels, columns = ['Decision tree', 'AdaBoost','MLP', 'KNN', 'SVM'])
+        best.index = [x * 0.1 for x in range(1, 10)]
 
 
         # printing test size from 0.1-1
@@ -128,16 +137,19 @@ if __name__ == '__main__':
 
 
         # how come some features without feature selection get same score? KNN...
-        print('models With all Features:')
+        print('Models With all the Features:')
         print("Decision Tree:\t\t Adaboost:\t\t\tMLP:\t\t\t\t KNN:\t\t\t\tSVM:")
-        print(bestTestAccuracy)
+        print(modelsWithAllFeatures)
 
-        print('models With Features:')
+        print('Models after feature selection:')
         print("Decision Tree:\t\t Adaboost:\t\t\tMLP:\t\t\t\t KNN:\t\t\t\tSVM:")
-        print(modelsWithFeatures)
+        print(bestTestAccuracyAfterFS)
+
+
         print(bestNumberOfFeatures)
 
         featuresSelected = pd.DataFrame(bestFeaturesMask, columns = ['Age', 'Sex','ChestPainType', 'RestingBP', 'Cholesterol','FastingBS', 'RestingECG','MaxHR', 'ExerciseAngina', 'Oldpeak','ST_Slope'])
+        featuresSelected.index = ['Decision tree', 'AdaBoost','MLP', 'KNN', 'SVM']
         disDF(featuresSelected.transpose())
 
         # plotModels()
